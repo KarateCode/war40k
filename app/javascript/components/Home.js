4ab6cf1e276dcf1589a14a8c1a023fe07510b63a
Michael Schneider <michael.schneider@envoyb2b.com>
2022-09-20 16:33:05 -0400

    Hookerized Home.js

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -1,18 +1,16 @@
 import React from 'react'
 
-class Home extends React.Component {
-	render () {
-		return (
-			<div className='Home'>
-				<h1>Warhammer 40,000</h1>
-				<menu>
-					<li><a href='/open_plays'>Open Play</a></li>
-					<li><a href='/armies'>Armies</a></li>
-					<li><a href='/matched_plays'>Matched Play</a></li>
-				</menu>
-			</div>
-		);
-	}
-}
+const Home  = () => {
+	return (
+		<div className='Home'>
+			<h1>Warhammer 40,000</h1>
+			<menu>
+				<li><a href='/open_plays'>Open Play</a></li>
+				<li><a href='/armies'>Armies</a></li>
+				<li><a href='/matched_plays'>Matched Play</a></li>
+			</menu>
+		</div>
+	);
+};
 
 export default Home
